29e005e46c11da55dbbda840384b9de7c1e4696e
sergi <frances1991@hotmail.com>
2017-12-27 18:15:25 +0100

    Añadida multiplicación

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -10,27 +10,35 @@ int main() {
         cout << "¿Qué operación de 2 números desea realizar?" << endl;
         cout << "1. Suma." << endl;
         cout << "2. Resta." << endl;
-        cout << "3. Salir." << endl;
+        cout << "3. Multiplicación." << endl;
+        cout << "4. Salir." << endl;
         cin >> opcion;
 
         switch(opcion) {
             case 1:
-                cout << "Intoduzca el pimer número: " << endl;
+                cout << "Introduzca el primer número: " << endl;
                 cin >> num1;
                 cout << "Introduzca el segundo número: " << endl;
                 cin >> num2;
                 cout << "El resultado es: " << num1+num2 << endl;
                 break;
             case 2:
-                cout << "Intoduzca el pimer número: " << endl;
+                cout << "Introduzca el primer número: " << endl;
                 cin >> num1;
                 cout << "Introduzca el segundo número: " << endl;
                 cin >> num2
                 cout << "El resultado es: " << num1-num2 << endl;
                 break;
+            case 3:
+                cout << "Introduzca el primer número: " << endl;
+                cin >> num1;
+                cout << "Introduzca el segundo número: " << endl;
+                cin >> num2;
+                cout << "El resultado es: " << num1*num2 << endl;
+                break;
 
         }
-    } while(opcion != 3);
+    } while(opcion != 4);
 
 
 
